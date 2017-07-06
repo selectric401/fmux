73362ff93c4bf43b318a29fbe8583fa04a0076d9
Lloyd W. Slade <me@lloydsla.de>
2017-07-05 20:47:31 -0400

    changed string names to look a bit less silly

--- tmux-right.sh
+++ tmux-right.sh
@@ -2,6 +2,6 @@
 UP=$(uptime | awk '{print $3}' | sed 's/,//')
 HOST=$(hostname -s)
 UTC=$(date -u +"%H:%M")
-UPSTRING=$(echo "↑ $UP")
-GMTSTRING=$(echo "UTC $UTC")
-echo '#[fg=colour249, bg=colour237] #('echo $GMTSTRING') #[fg=colour34, bg=colour237] #('echo $UPSTRING') #[fg=colour255 bg=colour34] #('echo $HOST') '
+STRING_UP=$(echo "↑ $UP")
+STRING_GMT=$(echo "UTC $UTC")
+echo '#[fg=colour249, bg=colour237] #('echo $STRING_GMT') #[fg=colour34, bg=colour237] #('echo $STRING_UP') #[fg=colour255 bg=colour34] #('echo $HOST') '
