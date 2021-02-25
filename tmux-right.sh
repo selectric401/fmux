5b9e97ecfd4fad268dc8a222bcad055e966acc70
Lloyd W. Slade <lloyd@chowderless.com>
2021-02-25 09:54:28 -0500

    removed a unicode character so this plays nicely with my vt320

--- tmux-right.sh
+++ tmux-right.sh
@@ -2,6 +2,6 @@
 UP=$(uptime | awk '{print $3}' | sed 's/,//')
 HOST=$(hostname -s)
 UTC=$(date -u +"%H:%M")
-STRING_UP=$(echo "↑ $UP")
+STRING_UP=$(echo "$UP")
 STRING_GMT=$(echo "UTC $UTC")
 echo '#('echo $STRING_GMT') #[fg=green] #('echo $STRING_UP') #[fg=black bg=green] #('echo $HOST') '
